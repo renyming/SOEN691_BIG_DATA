d5b846846c21cbf6d2e608358c4a5c50134c95a9
Zhiyi <dingzhiyi22@gmail.com>
2020-02-19 16:35:59 -0500

    Updata Code and dataSet

--- monitor.py
+++ monitor.py
@@ -8,10 +8,14 @@ Test_Instance = ['0', 'tcp', 'finger', 'S0', '0', '0', '0', '0', '0', '0', '0', 
                  '1', '0', '0', '0.31', '0.1', '0', '255', '52', '0.2', '0.04', '0',
                  '0', '1', '1', '0', '0', 'anomaly']
 
+
+
+
 def main():
     sc = SparkContext(appName="PysparkStreaming")
     ssc = StreamingContext(sc, 3)   #Streaming will execute in each 3 seconds
     #lines = ssc.textFileStream("hdfs://localhost:9000/input_dir")
+
     lines = ssc.textFileStream("./input_dir").map(lambda x: list(reader(StringIO(x))))\
                                              .map(lambda x: x[0])
 
@@ -22,11 +26,7 @@ def main():
 
 def calculate_distance(v1 , v2):
 
-
-
     return 0
-
-
 
 
 if __name__ == "__main__":
