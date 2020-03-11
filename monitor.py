aa0474e1d73c7196f1cce5bc404042b9351d5f83
Yiming Ren <renyming@gmail.com>
2020-03-11 15:16:23 -0400

    Update fix-sized instances pool in FIFO fashion

--- monitor.py
+++ monitor.py
@@ -39,18 +39,17 @@ def get_results(predictions_labels):
     return true_pos.union(false_pos.union(true_neg.union(false_neg)))
 
 
-def RT_KNN(sc , pool):
-
+def RT_KNN(sc, pool):
     ssc = StreamingContext(sc, 1)  # Streaming will execute in each 3 seconds
     # read on Hadoop
     # lines = ssc.textFileStream("hdfs://localhost:9000/input_dir")
 
     lines = ssc.textFileStream("./input_dir").map(lambda x:
-        list(reader(StringIO(x)))[0])
+                                                  list(reader(StringIO(x)))[0])
 
     # make predictions
     predictions_labels = lines.map(
-        lambda x : (real_time_KNN.KNN(pool, 10, x), x[-1]))
+        lambda x: (real_time_KNN.KNN(pool, 10, x), x[-1]))
 
     result = get_results(predictions_labels)
     result.pprint()
@@ -64,11 +63,10 @@ def RT_KNN(sc , pool):
 
 
 if __name__ == "__main__":
-
     # spark initialization
     conf = pyspark.SparkConf().setAppName("kmeans").setMaster("local[2]")
     sc = pyspark.SparkContext(appName="PysparkStreaming", conf=conf)
 
     KNN_pool = real_time_KNN.init_KNN('./source_dir/Train.csv', sc, 100)
 
-    RT_KNN(sc , KNN_pool)
+    RT_KNN(sc, KNN_pool)
