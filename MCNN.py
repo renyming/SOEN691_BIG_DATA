94c80949a891e3da4c1b6492dd9af60760a4239a
liyixuan <liyixuan4030614@gmail.com>
2020-03-26 20:05:56 -0400

    update code with preprocessing, correct mc split function

--- MCNN.py
+++ MCNN.py
@@ -7,30 +7,25 @@ from io import StringIO
 from os.path import join
 from csv import reader
 from mcnnmc import MC
-
 from datetime import datetime
 
 '''
-改动的地方:
-1. 每个mc文件头的信息：
+Notations: 
+1. information in each mc file:
     1) epsilon  : error counter
     2) n        : number of instances
     3) centroid : centroid
     4) CF2_x    : sum of the squares of the attributes in CF1_x
-2. 每个rdd需要读：epsilon, n, centroid, CF2_x
-    1) 先根据 centroid, n 算出 CF1_x
-    2) 在根据 CF1_x, CF2_x 算出 vairiance
-    3) 根据 centroid 算距离
-    4) 不需要存每个点的数据
-    5) Split : 找到最大的 variance 中的下标，先更新 CF1_x, 再根据 CF1_x 算出两个新的 
-               centroid 写回去
-    5) 如果用 foreachRDD，需要存下来:
-          epsilon
-          n 
-          centroid (CF1_x / n)
-          CF2_x
-    6) 需要在 MCNN/predict 方法里把 (prediction, label, time) 写到一个新的文件
-    7) 最后等所有 (prediction, label, time) 都存下来后，再写另一个py文件做 evaluation
+    5) activate/deactivate sign
+    6) filename
+2. steps after a rdd processes a stream:
+    1) calculate CF1_x based on centroid and n
+    2) calculate variance based on CF1_x and CF2_x
+    3) calculate distance based on instance and centroid
+    4) no necessary to store points
+    5) split : see function 'split_mc'
+    6) need to write (prediction, label, time) to a new file
+    7) evaluation
 '''
 
 mc_folder = './mcnn_mcs'
@@ -60,12 +55,12 @@ def clean_mc_folder():
 
 class MC_NN:
 
-    def __init__(self, theta):
+    def __init__(self, theta, mx):
         self.theta = theta
         self.pool = []  # a list of MC objects
         self.read_mcnn_pool()  # populate the essential attributes for MC
 
-        self.mx = 10    # maximum number of mcs
+        self.mx = mx    # maximum number of mcs
 
     def read_mcnn_pool(self):
         '''
@@ -86,17 +81,19 @@ class MC_NN:
             with open(join(mc_folder, file), 'r') as f:
                 lines = f.read().splitlines()
 
-                # the first line is epsilon (for convention)
                 mc.epsilon = int(lines[0])
                 mc.n = int(lines[1])  # count
-                mc.centroid = [float(x) for x in lines[2].split(',') if
-                               is_number(x)]
+                mc.centroid = [float(x) for x in lines[2].split(',') if is_number(x)]
                 mc.cf2_x = [float(x) for x in lines[3].split(',')]
                 mc.status = lines[4]  # status
                 mc.filename = lines[5]
 
-            mc.cf1_x = [x * mc.n for x in
-                        mc.centroid]  # The sum of the feature is the average centroid * n
+            mc.cf1_x = [x * mc.n for x in mc.centroid]  # The sum of the feature is the average centroid * n
+
+            # calculate the variance
+            temp_cf2_x = np.array(mc.cf2_x) / mc.n
+            temp_cf1_x = np.multiply(mc.centroid, mc.centroid)
+            mc.variance_x = np.sqrt(temp_cf2_x - temp_cf1_x).tolist()
 
             if mc.status == 'activate':
                 self.pool.append(mc)
@@ -125,35 +122,30 @@ class MC_NN:
 
         for mc in self.pool:
 
-            if (mc.epsilon > self.theta):
-                mc.status = 'deactivate'
-
+            if mc.epsilon > self.theta:
                 max_index = mc.variance_x.index(max(mc.variance_x))
                 max_variance = mc.variance_x[max_index]
 
                 child_mc_1 = MC(self.theta)
                 child_mc_2 = MC(self.theta)
 
-                child_mc_1.cl = mc.cl
-                child_mc_2.cl = mc.cl
+                child_mc_1.epsilon = int(mc.epsilon / 2)
+                child_mc_2.epsilon = int(mc.epsilon / 2)
+                child_mc_1.n = mc.n
+                child_mc_2.n = mc.n
 
                 child_mc_1.cf1_x = mc.cf1_x
-                child_mc_2.cf2_x = mc.cf1_x
-
-                child_mc_1.cf1_x[max_index] = max_variance
-                child_mc_2.cf2_x[max_index] = -max_variance
-
+                child_mc_2.cf1_x = mc.cf1_x
+                child_mc_1.cf1_x[max_index] += max_variance
+                child_mc_2.cf1_x[max_index] -= max_variance
                 child_mc_1.centroid = [x / mc.n for x in child_mc_1.cf1_x]
-                child_mc_2.centroid = [x / mc.n for x in child_mc_2.cf2_x]
+                child_mc_2.centroid = [x / mc.n for x in child_mc_2.cf1_x]
+                child_mc_1.cf2_x = [float(x) * float(y) for x, y in zip(child_mc_1.cf1_x, child_mc_1.cf1_x)]
+                child_mc_2.cf2_x = [float(x) * float(y) for x, y in zip(child_mc_2.cf1_x, child_mc_2.cf1_x)]
 
-                child_mc_1.cf2_x = [float(x) * float(y) for x, y in
-                                    zip(child_mc_1.cf1_x, child_mc_1.cf1_x)]
-                child_mc_2.cf2_x = [float(x) * float(y) for x, y in
-                                    zip(child_mc_2.cf2_x, child_mc_2.cf2_x)]
-
+                mc.status = 'deactivate'
                 child_mc_1.status = 'activate'
                 child_mc_2.status = 'activate'
-
                 child_mc_1.filename = 'child_1_' + mc.filename
                 child_mc_2.filename = 'child_2_' + mc.filename
 
@@ -173,7 +165,6 @@ class MC_NN:
             if distance < min_distance:
                 min_distance = distance
                 min_mc = mc
-
         prediction = min_mc.cl
 
         # update micro clusters and save on disk
@@ -226,7 +217,7 @@ class MC_NN:
             temp_cf1_x_2 = np.multiply(true_mc.centroid, true_mc.centroid)
             true_mc.variance_x = np.sqrt(temp_cf2_x - temp_cf1_x_2).tolist()
 
-            # TODO: check and split
+            # check and split
             if len(self.pool) < self.mx:
                 self.split_mc()
 
@@ -264,50 +255,41 @@ def init_mcnn_pool(data_file, sc):
     cf2_x_anomaly = None
 
     while normal is None or anomaly is None:
-        rand = data.takeSample(withReplacement=False, num=1,
-            seed=random.randint(0, 100))[0]
+        rand = data.takeSample(withReplacement=False, num=1, seed=random.randint(0, 100))[0]
 
         if rand[-1] == 'normal' and normal is None:
             normal = rand
-
             cf1_x_normal = [x for x in normal if is_number(x)]
-            cf2_x_normal = [float(x) * float(y) for x, y in
-                            zip(cf1_x_normal, cf1_x_normal)]
+            cf2_x_normal = [float(x) * float(y) for x, y in zip(cf1_x_normal, cf1_x_normal)]
 
         elif rand[-1] == 'anomaly' and anomaly is None:
             anomaly = rand
             cf1_x_anomaly = [x for x in anomaly if is_number(x)]
-            cf2_x_anomaly = [float(x) * float(y) for x, y in
-                             zip(cf1_x_anomaly, cf1_x_anomaly)]
+            cf2_x_anomaly = [float(x) * float(y) for x, y in zip(cf1_x_anomaly, cf1_x_anomaly)]
 
     with open(join(mc_folder, 'normal_mc_1.csv'), 'w', newline='') as f:
         csv_writer = csv.writer(f)
-        csv_writer.writerow("0")  # initial epsilon #first row
-        csv_writer.writerow(
-            "1")  # initial count of instances in the cluster #second row
-        csv_writer.writerow(
-            normal)  # initial centroid in the cluster #third row
+        csv_writer.writerow("0")        # initial epsilon #first row
+        csv_writer.writerow("1")        # initial count of instances in the cluster #second row
+        csv_writer.writerow(normal)     # initial centroid in the cluster #third row
         csv_writer.writerow(cf2_x_normal)  # initial cf2_x
         csv_writer.writerow(['activate'])  # initial mc status
         csv_writer.writerow(['normal_mc_1.csv'])
 
     with open(join(mc_folder, 'anomaly_mc_1.csv'), 'w', newline='') as f:
         csv_writer = csv.writer(f)
-        csv_writer.writerow("0")  # initial epsilon #first row
-        csv_writer.writerow(
-            "1")  # initial count of instances in the cluster #second row
-        csv_writer.writerow(
-            anomaly)  # initial centroid in the cluster #third row
+        csv_writer.writerow("0")        # initial epsilon #first row
+        csv_writer.writerow("1")        # initial count of instances in the cluster #second row
+        csv_writer.writerow(anomaly)    # initial centroid in the cluster #third row
         csv_writer.writerow(cf2_x_anomaly)  # initial cf2_x
-        csv_writer.writerow(['activate'])  # initial mc status
+        csv_writer.writerow(['activate'])   # initial mc status
         csv_writer.writerow(['anomaly_mc_1.csv'])
 
 
 def predict(instance):
-    mcnn = MC_NN(theta=20)
+    mcnn = MC_NN(theta=50, mx=10)
 
-    (prediction, true_lable, time_stamp) = mcnn.predict_and_update_mcs(
-        instance, instance[-1])
+    prediction, true_lable, time_stamp = mcnn.predict_and_update_mcs(instance, instance[-1])
 
     # TODO: after the previous RDD deletes the files, the next RDD may fail to
     #       read the mc files.
--- monitor.py
+++ monitor.py
@@ -1,4 +1,7 @@
+import sys
 import pyspark
+import numpy as np
+import pandas as pd
 import KNN as Knn
 from pyspark.streaming import StreamingContext
 from io import StringIO
@@ -6,8 +9,10 @@ from csv import reader
 from MCNN import predict
 from MCNN import init_mcnn_pool
 
+np.set_printoptions(threshold=sys.maxsize)
 
-def get_results(predictions_labels):
+
+def get_results(predictions_labels, sc):
     # 4 conditions
     #     normal:   positive
     #     abnormal: negative
@@ -38,6 +43,36 @@ def get_results(predictions_labels):
         false_neg = sc.parallelize(('false negative', 0))
 
     return true_pos.union(false_pos.union(true_neg.union(false_neg)))
+
+
+def preprocessing(path):
+    headers = ["duration", "protocol_type", "service", "flag", "src_bytes",
+               "dst_bytes", "land", "wrong_fragment", "urgent", "hot", "num_failed_logins",
+               "logged_in", "num_compromised", "root_shell", "su_attempted", "num_root",
+               "num_file_creations", "num_shells", "num_access_files", "num_outbound_cmds",
+               "is_host_login", "is_guest_login", "count", "srv_count", "serror_rate",
+               "srv_serror_rate", "rerror_rate", "srv_rerror_rate", "same_srv_rate",
+               "diff_srv_rate", "srv_diff_host_rate", "dst_host_count", "dst_host_srv_count",
+               "dst_host_same_srv_rate", "dst_host_diff_srv_rate", "dst_host_same_src_port_rate",
+               "dst_host_srv_diff_host_rate", "dst_host_serror_rate", "dst_host_srv_serror_rate",
+               "dst_host_rerror_rate", "dst_host_srv_rerror_rate", "target"]
+    df = pd.read_csv(path, header=None, names=headers)
+    mapping = {'normal': 0, 'anomaly': 1}
+    mapping_rev = {0: 'normal', 1: 'anomaly'}
+    df = df.replace({"target": mapping})
+    df = df._get_numeric_data()
+
+    # clean by iqr score
+    Q1 = df.quantile(0.03)
+    Q3 = df.quantile(0.97)
+    IQR = Q3 - Q1   # difference between 1th and 99th percentiles
+    df_clean = df[~((df < (Q1 - 1.5 * IQR)) | (df > (Q3 + 1.5 * IQR))).any(axis=1)]
+    print('before', df.shape)
+    print('after', df_clean.shape)
+
+    # write the cleaned data to another file 'source_dir/Train_c.csv'
+    df_clean = df_clean.replace({"target": mapping_rev})
+    df_clean.to_csv('./source_dir/Train_c.csv', index=False, header=False)
 
 
 def saveCoord(rdd):
@@ -82,11 +117,9 @@ if __name__ == "__main__":
     sc = pyspark.SparkContext(appName="PysparkStreaming", conf=conf)
     ssc = StreamingContext(sc, 1)  # Streaming will execute in each 3 seconds
 
-    KNN_pool = Knn.init_KNN('./source_dir/Train.csv', sc, 100)
-    init_mcnn_pool('./source_dir/Train.csv', sc)
-
-    # KNN_pool = Knn.init_KNN('./source_dir/Subsamples.csv', sc, 100)
-    # init_mcnn_pool('./source_dir/Subsamples.csv', sc)
+    preprocessing('./source_dir/Train.csv')
+    KNN_pool = Knn.init_KNN('./source_dir/Train_c.csv', sc, 100)
+    init_mcnn_pool('./source_dir/Train_c.csv', sc)
 
     main(ssc , KNN_pool)
 
