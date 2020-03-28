efb2fbd2c775233d4ec44478df06c3deade5dd16
Yiming Ren <renyming@gmail.com>
2020-03-28 12:19:32 -0400

    Minimize distance heap size

--- KNN_classical.py
+++ KNN_classical.py
@@ -4,13 +4,13 @@ from csv import reader
 import heapq
 
 
-def vote(instance, train, k=100):
+def vote(instance, train, k):
     votes = []
     for t in train:
-        dist = distance(t, instance)
+        dist = -distance(t, instance)
         heapq.heappush(votes, (dist, t))
-
-    votes = heapq.nsmallest(k, votes)
+        if len(votes)>k:
+            heapq.heappop(votes)
 
     cnt_normal = 0
     cnt_anomaly = 0
@@ -45,37 +45,46 @@ def is_float(v):
 
 
 if __name__ == '__main__':
-    conf = pyspark.SparkConf().setMaster("local[*]")
+    conf = pyspark.SparkConf().setMaster("local[1]")
     sc = pyspark.SparkContext(appName="ClassicalKNN", conf=conf)
 
     data = sc.textFile('./input_dir/raw_data[0-5].csv').map(lambda x: list(reader(StringIO(x)))[0])
     test, train = data.randomSplit(weights=[0.8, 0.2])
     train = train.collect()
 
-    pred_and_labels = test.map(lambda x: (vote(x, train, k=10), x[-1]))
+    k_range = range(3, 26)
 
-    true_pos = pred_and_labels.filter \
-        (lambda x: x[0] == x[1] and x[0] == 'anomaly') \
-        .map(lambda x: ('True positive', 1)) \
-        .reduceByKey(lambda x, y: x + y).collect()[0][1]
-    false_pos = pred_and_labels \
-        .filter(lambda x: x[0] != x[1] and x[0] == 'anomaly') \
-        .map(lambda x: ('False positive', 1)) \
-        .reduceByKey(lambda x, y: x + y).collect()[0][1]
-    true_neg = pred_and_labels \
-        .filter(lambda x: x[0] == x[1] and x[0] == 'normal') \
-        .map(lambda x: ('True negative', 1)) \
-        .reduceByKey(lambda x, y: x + y).collect()[0][1]
-    false_neg = pred_and_labels \
-        .filter(lambda x: x[0] != x[1] and x[0] == 'normal') \
-        .map(lambda x: ('False negative', 1)) \
-        .reduceByKey(lambda x, y: x + y).collect()[0][1]
+    for k in k_range:
 
-    accuracy = (true_pos + true_neg) / (true_pos + true_neg + false_pos + false_neg)
-    precision = true_pos / (true_pos + false_pos)
-    recall = true_pos / (true_pos + false_neg)
+        pred_and_labels = test.map(lambda x: (vote(x, train, k), x[-1]))
 
-    print("Accuracy: {:0.5f}".format(accuracy))
-    print("Precision: {:0.5f}".format(precision))
-    print("Recall: {:0.5f}".format(recall))
-    print("F1 Measure: {:0.5f}".format((2 * precision * recall / (precision + recall))))
+        anomaly = pred_and_labels.filter(lambda x: x[0] == 'anomaly')
+        normal = pred_and_labels.filter(lambda x: x[0] == 'normal')
+
+        true_pos = anomaly \
+            .filter(lambda x: x[0] == x[1]) \
+            .map(lambda x: ('True positive', 1)) \
+            .reduceByKey(lambda x, y: x + y).collect()[0][1]
+        false_pos = anomaly \
+            .filter(lambda x: x[0] != x[1]) \
+            .map(lambda x: ('False positive', 1)) \
+            .reduceByKey(lambda x, y: x + y).collect()[0][1]
+        true_neg = normal \
+            .filter(lambda x: x[0] == x[1]) \
+            .map(lambda x: ('True negative', 1)) \
+            .reduceByKey(lambda x, y: x + y).collect()[0][1]
+        false_neg = normal \
+            .filter(lambda x: x[0] != x[1]) \
+            .map(lambda x: ('False negative', 1)) \
+            .reduceByKey(lambda x, y: x + y).collect()[0][1]
+
+        accuracy = (true_pos + true_neg) / (true_pos + true_neg + false_pos + false_neg)
+        precision = true_pos / (true_pos + false_pos)
+        recall = true_pos / (true_pos + false_neg)
+
+        print("K: {:d}".format(k))
+        print("Accuracy: {:0.5f}".format(accuracy))
+        print("Precision: {:0.5f}".format(precision))
+        print("Recall: {:0.5f}".format(recall))
+        print("F1 Measure: {:0.5f}".format((2 * precision * recall / (precision + recall))))
+        print("---------------------------------------")
