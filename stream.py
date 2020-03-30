9b706816dd6434041c625109ba3f691219758dde
Zhiyi <dingzhiyi22@gmail.com>
2020-03-29 21:10:43 -0400

    update on stream

--- stream.py
+++ stream.py
@@ -1,36 +1,42 @@
 import csv
 import time
+import math
 import os, shutil
 
 
 def start(nb_instances, sleep_time):
     raw_data = []
-    with open('./source_dir/Train_clean.csv', 'r') as csv_file:
+    with open('./source_dir/Train_c.csv', 'r') as csv_file:
 
         csv_reader = csv.reader(csv_file)
 
         for line in csv_reader:
             raw_data.append(line)
 
+    total_size_data = len(raw_data)
+    nb_instance_per_batch = nb_instances
+    nb_batches = math.ceil(total_size_data / nb_instance_per_batch)
 
     start_index = 0
 
-    for i in range(200):
+    for i in range(nb_batches):
 
         with open('./input_dir/raw_data' + str(i) + '.csv', 'w', newline='') as file:
 
             csv_writer = csv.writer(file)
-            #ri = random.randint(40,50)
 
-            ri = 100
+            if total_size_data >= nb_instance_per_batch:
+                nb_instance_per_batch = nb_instances
+            else:
+                nb_instance_per_batch = total_size_data % nb_instances
 
-            for j in range(ri):
+            for j in range(nb_instance_per_batch):
 
                 csv_writer.writerow(raw_data[j + start_index])
 
-        start_index += ri
-
-        time.sleep(3)
+        start_index += nb_instance_per_batch
+        total_size_data -= nb_instance_per_batch
+        time.sleep(sleep_time)
 
 def clean():
 
@@ -45,8 +51,6 @@ def clean():
                 shutil.rmtree(file_path)
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
-
-clean()
 
 
 def start_stream():
