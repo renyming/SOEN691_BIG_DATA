c2c6f4cc6094985f8096884349833283d657d104
liyixuan <liyixuan4030614@gmail.com>
2020-03-29 20:38:07 -0400

    modify preprocessing, add package install commands for normalization

--- monitor.py
+++ monitor.py
@@ -1,6 +1,7 @@
 import pyspark
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import KNN as Knn
 from sklearn import preprocessing
 from sklearn.utils import shuffle
@@ -12,23 +13,22 @@ from MCNN import init_mcnn_pool
 
 
 def get_results(predictions_labels, sc):
-    # 4 conditions
-    #     normal:   positive
-    #     abnormal: negative
+    # anomaly: positive
+    # normal: negative
     true_pos = predictions_labels \
-        .filter(lambda x: x[0] == x[1] and x[0] == 'normal') \
+        .filter(lambda x: x[0] == x[1] and x[0] == 'anomaly') \
         .map(lambda x: ('True positive', 1)) \
         .reduceByKey(lambda x, y: x + y)
     false_pos = predictions_labels \
-        .filter(lambda x: x[0] != x[1] and x[0] == 'normal') \
+        .filter(lambda x: x[0] != x[1] and x[0] == 'anomaly') \
         .map(lambda x: ('False positive', 1)) \
         .reduceByKey(lambda x, y: x + y)
     true_neg = predictions_labels \
-        .filter(lambda x: x[0] == x[1] and x[0] == 'anomaly') \
+        .filter(lambda x: x[0] == x[1] and x[0] == 'normal') \
         .map(lambda x: ('True negative', 1)) \
         .reduceByKey(lambda x, y: x + y)
     false_neg = predictions_labels \
-        .filter(lambda x: x[0] != x[1] and x[0] == 'anomaly') \
+        .filter(lambda x: x[0] != x[1] and x[0] == 'normal') \
         .map(lambda x: ('False negative', 1)) \
         .reduceByKey(lambda x, y: x + y)
 
@@ -58,8 +58,18 @@ def data_preprocessing(path):
     df = pd.read_csv(path, header=None, names=headers)
     df_normal = df.loc[df['target'] == 'normal']
     df_anomaly = df.loc[df['target'] == 'anomaly']
-
-    # First, find the outliers in normal and anomaly and ignore them
+    def plot_data(sdf, title):
+        x_axis = 'src_bytes'
+        y_axis = 'dst_bytes'
+        fig, ax = plt.subplots(figsize=(6, 6))
+        clean_data_normal = sdf.loc[sdf['target'] == 'normal']
+        clean_data_anomaly = sdf.loc[sdf['target'] == 'anomaly']
+        ax.scatter(clean_data_normal[x_axis], clean_data_normal[y_axis], s=2, c='red')
+        ax.scatter(clean_data_anomaly[x_axis], clean_data_anomaly[y_axis], s=2, c='blue')
+        ax.set_xlabel(x_axis)
+        ax.set_ylabel(y_axis)
+        plt.title(title)
+        plt.show()
     def filter_outliers(dataf):
         # calc iqr score
         Q1 = dataf._get_numeric_data().quantile(0.001)
@@ -68,22 +78,24 @@ def data_preprocessing(path):
         # clean by iqr score
         df_class_clean = dataf[~((dataf._get_numeric_data() < (Q1 - 1.5 * IQR)) | (dataf._get_numeric_data() > (Q3 + 1.5 * IQR))).any(axis=1)]
         return df_class_clean
+
+    # First, find the outliers in normal and anomaly and ignore them
+    # plot_data(df, 'before filtering')
     df_normal_clean = filter_outliers(df_normal)
     df_anomaly_clean = filter_outliers(df_anomaly)
     df_clean = pd.concat([df_normal_clean, df_anomaly_clean], axis=0, sort=False)
-
+    # plot_data(df_clean, 'after filtering')
     print('preprocessing:')
-    print('  previous', df.shape)
-    print('  later   ', df_clean.shape)
+    print('  ', df.shape)
+    print('  ', df_clean.shape)
     # Second, normalize all numeric columns
     for column in df_clean._get_numeric_data():
         mmscaler = preprocessing.MinMaxScaler()
         x_array = np.array(df_clean[column].astype(float)).reshape(-1, 1)
         df_clean[column] = mmscaler.fit_transform(x_array)
-    # set normalized clean dataset to df
     df_norm = df_clean
-
-    # Finally, shuffle the data then write the cleaned data to file 'source_dir/Train_clean.csv'
+    # plot_data(df_norm, 'after normalizing')
+    # shuffle the data then write the clean data to 'Train_clean.csv'
     df_final = shuffle(df_norm)
     df_final.to_csv('./source_dir/Train_clean.csv', index=False, header=False)
 
