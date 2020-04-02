ee05300efccb25517dc8ecadf3a84b48dd09b9eb
Yiming Ren <renyming@gmail.com>
2020-04-01 22:17:54 -0400

    Test cases union only once

--- KNN_classical.py
+++ KNN_classical.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pyspark import SparkContext, SparkConf
 from io import StringIO
 from csv import reader
@@ -43,29 +44,31 @@ def output_to_file(out_text):
 
 
 if __name__ == '__main__':
-    # SparkContext.setSystemProperty('spark.executor.memory', '2500m')
     conf = SparkConf().setMaster("local[*]")
     sc = SparkContext(appName="ClassicalKNN", conf=conf)
 
     data = sc.textFile('./source_dir/Train_clean.csv').map(lambda x: list(reader(StringIO(x)))[0])
     n_folds = 5
     folds = data.randomSplit(weights=[1.0 / n_folds] * n_folds)
-    for fold in folds:
-        fold.cache()
 
-    k_range = range(4, 11)
+    train_list = []
+    test_list = []
+    for n in range(n_folds):
+        train_list.append(folds[n])
+        test_list.append(sc.parallelize([]))
+        for i in range(n_folds):
+            if i != n:
+                test_list[n] = test_list[n].union(folds[i])
+
+    k_range = range(3, 11)
 
     for k in k_range:
         output_to_file("========================================================\n")
         for n in range(n_folds):
-            train = folds[n].collect()
-            pred_and_labels = []
+            train = train_list[n].collect()
+            test = test_list[n]
 
-            for i in range(n_folds):
-                if i != n:
-                    pred_and_labels.extend(folds[i].map(lambda x: (vote(x, train, k), x[-1])).collect())
-
-            pred_and_labels = sc.parallelize(pred_and_labels)
+            pred_and_labels = test.map(lambda x: (vote(x, train, k), x[-1]))
 
             true_pos = pred_and_labels \
                 .filter(lambda x: x[0] == x[1] and x[0] == 'anomaly') \
@@ -88,8 +91,7 @@ if __name__ == '__main__':
             precision = true_pos / (true_pos + false_pos)
             recall = true_pos / (true_pos + false_neg)
 
-            output = ""
-
+            output = "Time: {:s}\n".format(str(datetime.now()))
             output += "K: {:d}\n".format(k)
             output += "Fold: {:d}\n".format(n+1)
             output += "True pos: {:d}\n".format(true_pos)
